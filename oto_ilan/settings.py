bc8ad6b06b0d39db44811e915fa1c0f1bbc3559c
GhostFaceInterface <m.besiraslan40@gmail.com>
2025-04-25 21:51:10 +0300

    Adding pagination

--- oto_ilan/settings.py
+++ oto_ilan/settings.py
@@ -149,5 +149,8 @@ REST_FRAMEWORK = {
         "rest_framework.filters.OrderingFilter",
     ],
 
+    "DEFAULT_PAGİNATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 30,
+
 
 }

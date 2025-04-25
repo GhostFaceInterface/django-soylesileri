ff9bff427dab7b3c2e49260cd4e2ac7d9e243119
GhostFaceInterface <m.besiraslan40@gmail.com>
2025-04-25 22:02:20 +0300

    messages permissions

--- private_messages/views.py
+++ private_messages/views.py
@@ -8,6 +8,27 @@ class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+
+    def get_queryset(self): #Şu an giriş yapmış kullanıcı kimse onunla ilgili mesajları getir
+        
+        # Kullanıcıyı request nesnesinden alıyoruz. 
+        # self.request.user, şu anda oturum açmış olan kullanıcıyı temsil eder.
+        user = self.request.user
+        # Mesajları filtrelemek için iki farklı sorgu oluşturuyoruz:
+        # 1. Kullanıcının gönderici olduğu mesajlar (sender=user)
+        # 2. Kullanıcının alıcı olduğu mesajlar (receiver=user)
+        # Bu iki sorguyu '|' operatörü ile birleştiriyoruz. 
+        # '|' operatörü, Django ORM'de iki sorguyu birleştirerek bir "veya" (OR) işlemi yapar.
+        # (sender == request.user) OR (receiver == request.user)
+        # Bu sayede, kullanıcının hem gönderici hem de alıcı olduğu mesajları tek bir sorgu ile almış oluyoruz.
+
+        return Message.objects.filter(
+            sender=user # Kullanıcının gönderici olduğu mesajlar
+        ) | Message.objects.filter(
+            receviver=user # Kullanıcının alıcı olduğu mesajlar
+        )
+        
+
     def perform_create(self, serializer):
        serializer.save(sender=self.request.user)
 # Create your views here.
